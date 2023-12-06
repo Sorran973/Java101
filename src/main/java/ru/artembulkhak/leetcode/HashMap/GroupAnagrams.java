4dc44e01eca804c773f7b5cc07fd5d06f0ab32f1
Artem Bulkhak <artem.bulkhak@mail.ru>
2023-12-06 23:13:21 +0300

    Added "Group Anagrams" problem
    Solutions:
    - using HashMap<sorted_String, List<String>> - any string
    from the input array convert into char[], then sort, then convert
    back into string

---
+++ src/main/java/ru/artembulkhak/leetcode/HashMap/GroupAnagrams.java
@@ -0,0 +1,33 @@
+package ru.artembulkhak.leetcode.HashMap;
+
+import java.util.*;
+
+/**
+ * https://leetcode.com/problems/group-anagrams/description/
+ */
+
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+
+            map.get(sortedWord).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new GroupAnagrams().groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+        System.out.println(new GroupAnagrams().groupAnagrams(new String[]{"ddddddddddg","dgggggggggg"}));
+    }
+}
