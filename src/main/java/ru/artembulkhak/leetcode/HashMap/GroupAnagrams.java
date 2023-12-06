a18c96258793a016dd80d8bf0a6c10c753053163
Artem Bulkhak <artem.bulkhak@mail.ru>
2023-12-07 00:15:50 +0300

    Added "Group Anagrams" second solution
    Solutions:
    - using HashMap<sorted_String, List<String>> - any string
    from the input array convert into char[], then sort, then convert
    back into string
    - using an array[26] of ASCII characters

--- src/main/java/ru/artembulkhak/leetcode/HashMap/GroupAnagrams.java
+++ src/main/java/ru/artembulkhak/leetcode/HashMap/GroupAnagrams.java
@@ -26,6 +26,44 @@ public class GroupAnagrams {
         return new ArrayList<>(map.values());
     }
 
+    public int[] fillASCIIArray(String str) {
+        int[] arr = new int[26];
+        for (char c: str.toCharArray()) {
+            arr[(int)c - (int)'a'] += 1;
+        }
+
+        return arr;
+    }
+
+    public List<List<String>> groupAnagramsASCII(String[] strs) {
+        List<int[]> arrayList = new ArrayList<>();
+        List<List<String>> result = new ArrayList<>();
+        int[] baseArr = fillASCIIArray(strs[0]);
+        arrayList.add(baseArr);
+        result.add(new ArrayList<>());
+        result.get(0).add(strs[0]);
+
+        for (int i = 1; i < strs.length; i++) {
+            boolean flag = true;
+            int[] arr = fillASCIIArray(strs[i]);
+            for (int j = 0; j < arrayList.size(); j++) {
+                if (Arrays.equals(arr, arrayList.get(j))) {
+                    result.get(j).add(strs[i]);
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) {
+                arrayList.add(arr);
+                result.add(new ArrayList<>());
+                result.get(result.size()-1).add(strs[i]);
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println(new GroupAnagrams().groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
         System.out.println(new GroupAnagrams().groupAnagrams(new String[]{"ddddddddddg","dgggggggggg"}));
