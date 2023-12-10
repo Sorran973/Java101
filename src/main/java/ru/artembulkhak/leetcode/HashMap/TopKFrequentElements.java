b3036e3dfb062aca1827077372ca66a223b6280e
Artem Bulkhak <artem.bulkhak@mail.ru>
2023-12-10 22:53:29 +0300

    Added "Top K Frequent Elements" additional solutions
    Solutions:
    - (the best) using HashMap and Bucket Sort: calculate frequency of
    array elements using HashMap; serve keys from map.keySet() into the
    bucket
    array; take result from the bucket
    - using HashMap and Comparator
    - using PriorityQueue that also has a comparator in its constructor
    (faster than via HashMap)

--- src/main/java/ru/artembulkhak/leetcode/HashMap/TopKFrequentElements.java
+++ src/main/java/ru/artembulkhak/leetcode/HashMap/TopKFrequentElements.java
@@ -7,13 +7,54 @@ import java.util.*;
 
 public class TopKFrequentElements {
 
-    public int[] topKFrequentHashMap(int[] nums, int k) {
+    public int[] topKFrequentPriorityQueue(int[] nums, int k) {
+        int[] result = new int[k];
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for (int i: nums) {
+            map.put(i,map.getOrDefault(i,0) + 1);
+        }
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
+        priorityQueue.addAll(map.keySet());
+
+        for (int i = 0; i < k; i++) {
+            result[i] = priorityQueue.poll();
+        }
+
+        return result;
+    }
+
+    public int[] topKFrequentHashMapComparator(int[] nums, int k) {
+        int[] result = new int[k];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i: nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+//      Faster approach:
+        List<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((a, b) -> map.get(b) - map.get(a));
+
+//      Slower approach:
+//        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+//        list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+        for (int i = 0; i < k; i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+
+    public int[] topKFrequentBucketSort(int[] nums, int k) {
         int[] result = new int[k];
         List<Integer>[] buckets = new ArrayList[nums.length + 1];
         Map<Integer, Integer> map = new HashMap<>();
 
-        for(int n : nums){
-            map.put(n,map.getOrDefault(n,0) + 1);
+        for(int i: nums){
+            map.put(i,map.getOrDefault(i,0) + 1);
         }
 
         for (int key: map.keySet()) {
@@ -41,6 +82,6 @@ public class TopKFrequentElements {
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new TopKFrequentElements().topKFrequentHashMap(new int[]{3,0,1,0}, 1)));
+        System.out.println(Arrays.toString(new TopKFrequentElements().topKFrequentBucketSort(new int[]{3,0,1,0}, 1)));
     }
 }
