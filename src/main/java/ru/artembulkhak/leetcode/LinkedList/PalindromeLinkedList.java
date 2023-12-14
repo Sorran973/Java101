39da7a5d408cc50b261def994fd57f8e26a23fef
Artem Bulkhak <artem.bulkhak@mail.ru>
2023-12-14 20:32:21 +0300

    Added "Palindrome Linked List" second solution
    Solutions:
    - (the best) with Two Pointers (fast and slow) find the middle of the
    Linked List; then manually Reverse the second half of the list and
    equal them
    - traverse Linked List and put all values of nodes in List; then check
    the List by Two Pointers from both sides

--- src/main/java/ru/artembulkhak/leetcode/LinkedList/PalindromeLinkedList.java
+++ src/main/java/ru/artembulkhak/leetcode/LinkedList/PalindromeLinkedList.java
@@ -21,6 +21,36 @@ public class PalindromeLinkedList {
         }
     }
 
+    public boolean isPalindromeTwoPointersAndReverse(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        while (slow.next != null) {
+            ListNode tmp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = tmp;
+        }
+        slow.next = prev;
+
+
+        while (slow != null) {
+            if (slow.val != head.val) {
+                return false;
+            }
+            slow = slow.next;
+            head = head.next;
+        }
+
+        return true;
+    }
+
 
     public boolean isPalindromeViaList(ListNode head) {
         if (head == null) {
@@ -52,17 +82,23 @@ public class PalindromeLinkedList {
 
 
     public static void main(String[] args) {
-        int n = 2;
+        int n = 3;
         PalindromeLinkedList.ListNode[] arr1 = new PalindromeLinkedList.ListNode[n];
 
         arr1[0] = new PalindromeLinkedList.ListNode(1);
         arr1[1] = new PalindromeLinkedList.ListNode(2);
-//        arr1[2] = new PalindromeLinkedList.ListNode(2);
+        arr1[2] = new PalindromeLinkedList.ListNode(1);
 //        arr1[3] = new PalindromeLinkedList.ListNode(1);
+//        arr1[4] = new PalindromeLinkedList.ListNode(5);
+//        arr1[5] = new PalindromeLinkedList.ListNode(4);
+//        arr1[6] = new PalindromeLinkedList.ListNode(3);
+//        arr1[7] = new PalindromeLinkedList.ListNode(2);
+//        arr1[8] = new PalindromeLinkedList.ListNode(1);
+
 
         for (int i = 0; i < n-1; i++) {
             arr1[i].next = arr1[i+1];
         }
-        System.out.println(new PalindromeLinkedList().isPalindromeViaList(arr1[0]));
+        System.out.println(new PalindromeLinkedList().isPalindromeTwoPointersAndReverse(arr1[0]));
     }
 }
