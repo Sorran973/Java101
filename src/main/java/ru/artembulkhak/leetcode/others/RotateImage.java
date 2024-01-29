3713c3ff3d9b7ad55f46334d0c7a6da8d75d59f6
Artem Bulkhak <artem.bulkhak@mail.ru>
2024-01-29 15:42:43 +0300

    Refactoring in rotateThroughTranspose method

--- src/main/java/ru/artembulkhak/leetcode/others/RotateImage.java
+++ src/main/java/ru/artembulkhak/leetcode/others/RotateImage.java
@@ -15,9 +15,7 @@ public class RotateImage {
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
             }
-        }
 
-        for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length/2; j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[i][matrix.length - 1 - j];
