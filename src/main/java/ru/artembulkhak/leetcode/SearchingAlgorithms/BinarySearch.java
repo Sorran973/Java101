478c93af4c5deb8f681c5d9b6c359cd1fc438b68
Artem Bulkhak <artem.bulkhak@mail.ru>
2024-02-03 10:45:55 +0300

    Added "Binary Search" problem second solution
    Solution:
    - the same scheme but through recursion

--- src/main/java/ru/artembulkhak/leetcode/SearchingAlgorithms/BinarySearch.java
+++ src/main/java/ru/artembulkhak/leetcode/SearchingAlgorithms/BinarySearch.java
@@ -27,6 +27,27 @@ public class BinarySearch {
         return result;
     }
 
+
+    // Time: O(logn); Space: O(logn) because of Call Stack
+    static int recursiveBinarySearch(int[] nums, int target) {
+        return search(nums, target, 0, nums.length - 1);
+    }
+    static int search(int[] sortedArray, int target, int start, int end) {
+        if (start <= end) {
+            int middle = (end - start) / 2 + start;
+            if (sortedArray[middle] == target) {
+                return middle;
+            } else if (sortedArray[middle] > target) {
+                return search(sortedArray, target, start, middle - 1);
+            } else {
+                return search(sortedArray, target, middle + 1, end);
+            }
+        }
+
+        return -1;
+    }
+
+
     public static void main(String[] args) {
         int[] arr = new int[]{-1,0,3,5,9,12};
         System.out.println(binarySearch(arr, 0));
