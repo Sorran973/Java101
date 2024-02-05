89dff1cadb3247ca6af21ff9bcd942062139d68d
Artem Bulkhak <artem.bulkhak@mail.ru>
2024-02-05 12:08:55 +0300

    Added "Jump Game" second solution (not good)
    Solution:
    - through recursion. But time limit exceeded because of depth of
    recursion stack

--- src/main/java/ru/artembulkhak/leetcode/others/JumpGame.java
+++ src/main/java/ru/artembulkhak/leetcode/others/JumpGame.java
@@ -6,6 +6,38 @@ package ru.artembulkhak.leetcode.others;
 
 public class JumpGame {
 
+    // Time Limit Exceeded because of Depth of Recursion Stack
+    static boolean canJumpRecursion (int[] nums) {
+        if (nums[0] == 0) {
+            return false;
+        }
+
+        for (int i = 1; i <= nums[0]; i++) {
+            if (!jump(nums, i)) {
+                continue;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static boolean jump (int[] nums, int i) {
+        if (i == nums.length - 1) {
+            return true;
+        }
+
+        for (int j = 1; j <= nums[i]; j++) {
+            if (!jump(nums, i + j)) {
+                continue;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     static boolean canJump (int[] nums) {
         if (nums[0] == 0 && nums.length != 1) {
             return false;
@@ -35,7 +67,8 @@ public class JumpGame {
 
 
     public static void main(String[] args) {
-        System.out.println(canJump(new int[]{1, 2, 3, 0, 4}));
-        System.out.println(canJump(new int[]{1, 0, 3, 0, 4}));
+//        System.out.println(canJumpRecursion(new int[]{1, 2, 3, 0, 4}));
+//        System.out.println(canJumpRecursion(new int[]{0, 0, 0, 1, 4}));
+        System.out.println(canJumpRecursion(new int[]{2, 0, 3, 0, 4}));
     }
 }
